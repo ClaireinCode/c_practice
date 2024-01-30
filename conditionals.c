4e12b725d23a10215e13f2e507d01f2fc608d5c2
Claireese Underwood <claireese.underwood@gmail.com>
2024-01-30 15:20:59 -0600

    javascript kept a lot from c. typescript should be much easier than I thought

--- conditionals.c
+++ conditionals.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     int x = 0;
     int y = 5;
 
@@ -15,5 +16,22 @@ int main(void){
     else
     {
         printf("x is equal to y\n");
+    }
+
+    printf("\nDo you agree to the terms and conditions?\n");
+
+    char c = 'Y';
+
+    if (c == 'Y' || c =='y')
+    {
+        printf("Agreed\n");
+    }
+    else if (c == 'N' || c == 'n')
+    {
+        printf("Does not agree\n");
+    }
+    else
+    {
+        printf("meow. That doesn't work!\n");
     }
 }
