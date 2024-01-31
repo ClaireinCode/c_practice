ec9d4f2ee080e05666d3269a5e757d1fdd652eb4
Claireese Underwood <claireese.underwood@gmail.com>
2024-01-30 18:26:18 -0600

    played with loops

---
+++ loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+    while (i > 0)
+    {
+        printf("bark\n");
+        i--;
+    }
+
+    for (int k = 0; k < 5; k++)
+    {
+        printf("meow\n");
+    }
+
+}
